090b3403bf1320d0ed1a009bbecbb578dd5d3665
Daria Dolinovskaya <dolinovskaya98gmail.com>
2023-09-05 20:54:51 +0300

    fix errors 404

--- controllers/users.js
+++ controllers/users.js
@@ -22,15 +22,12 @@ module.exports.getUsers = (req, res) => {
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => {
-      if (!user) {
-        res.status(404).send({ message: 'Пользователя с данным id не найден' });
-        return;
-      }
-      res.send(user);
-    })
+    .orFail(new Error('NotFoundId'))
+    .then((user) => { res.status(200).send(user); })
     .catch((err) => {
-      if (err.name === 'CastError') {
+      if (err.message === 'NotFoundId') {
+        res.status(404).send({ message: 'Пользователя с данным id не найден' });
+      } else if (err.name === 'CastError') {
         res.status(400).send({ message: 'Некорректный id' });
       } else {
         res.status(500).send({ message: 'Ошибка на сервере' });
@@ -40,34 +37,32 @@ module.exports.getUserById = (req, res) => {
 
 module.exports.editUserData = (req, res) => {
   const { name, about } = req.body;
-  if (req.user._id) {
-    User.findByIdAndUpdate(req.user._id, { name, about }, { new: 'true', runValidators: true }) // new - для возврата обновленных данных, runValidators - для валидации полей
-      .then((user) => res.send(user))
-      .catch((err) => {
-        if (err.name === 'ValidationError') {
-          res.status(400).send({ message: err.message });
-        } else {
-          res.status(404).send({ message: 'Пользователя с данным id не найден' });
-        }
-      });
-  } else {
-    res.status(500).send({ message: 'Ошибка на сервере' });
-  }
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: 'true', runValidators: true }) // new - для возврата обновленных данных, runValidators - для валидации полей
+    .orFail(new Error('NotFoundId'))
+    .then((user) => { res.send(user); })
+    .catch((err) => {
+      if (err.message === 'NotFoundId') {
+        res.status(404).send({ message: 'Пользователя с данным id не найден' });
+      } else if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: 'Ошибка на сервере' });
+      }
+    });
 };
 
 module.exports.editUserAvatar = (req, res) => {
   const { avatar } = req.body;
-  if (req.user._id) {
-    User.findByIdAndUpdate(req.user._id, { avatar }, { new: 'true', runValidators: true })
-      .then((user) => res.send(user))
-      .catch((err) => {
-        if (err.name === 'ValidationError') {
-          res.status(400).send({ message: err.message });
-        } else {
-          res.status(404).send({ message: 'Пользователя с данным id не найден' });
-        }
-      });
-  } else {
-    res.status(500).send({ message: 'Ошибка на сервере' });
-  }
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: 'true', runValidators: true })
+    .orFail(new Error('NotFoundId'))
+    .then((user) => { res.send(user); })
+    .catch((err) => {
+      if (err.message === 'NotFoundId') {
+        res.status(404).send({ message: 'Пользователя с данным id не найден' });
+      } else if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: 'Ошибка на сервере' });
+      }
+    });
 };
