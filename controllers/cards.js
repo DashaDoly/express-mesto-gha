c939882deeeb262f919a2f966a180aa602ee7578
Daria Dolinovskaya <dolinovskaya98gmail.com>
2023-09-08 02:25:02 +0300

    14: added sign in/up - fix errors 2

--- controllers/cards.js
+++ controllers/cards.js
@@ -42,29 +42,44 @@ module.exports.deleteCard = (req, res, next) => {
         .orFail()
         .then(() => { res.status(HTTP_STATUS_OK).send({ message: 'Карточка удалена' }); })
         .catch((err) => {
+          next(err);
+          // if (err instanceof mongoose.Error.DocumentNotFoundError) {
+          //   next(new NotFoundError('Карточка с данным id не найдена'));
+          // } else if (err instanceof mongoose.Error.CastError) {
+          //   next(new BadRequestError('Некорректный id карточки'));
+          // } else {
+          //   next(err);
+          // }
           console.log(err);
-          switch (err.constructor) {
-            case mongoose.Error.DocumentNotFoundError:
-              next(new NotFoundError('Карточка с данным id не найдена'));
-              break;
-            case mongoose.Error.CastError:
-              next(new BadRequestError('Некорректный id карточки'));
-              break;
-            default:
-              next(err);
-              break;
-          }
+          // switch (err.constructor) {
+          //   case mongoose.Error.DocumentNotFoundError:
+          //     next(new NotFoundError('Карточка с данным id не найдена'));
+          //     break;
+          //   case mongoose.Error.CastError:
+          //     next(new BadRequestError('Некорректный id карточки'));
+          //     break;
+          //   default:
+          //     next(err);
+          //     break;
+          // }
         });
     })
     .catch((err) => {
-      console.log(err);
       if (err.name === 'TypeError') {
         next(new NotFoundError('Карточка с данным id не найдена'));
-      // } else if (mongoose.Error.CastError) {
-      //   next(new BadRequestError('Некорректный id карточки'));
+      } else if (err instanceof mongoose.Error.CastError) {
+        next(new BadRequestError('Некорректный id карточки'));
       } else {
         next(err);
       }
+      console.log(err);
+      // if (err.name === 'TypeError') {
+      //   next(new NotFoundError('Карточка с данным id не найдена'));
+      // } else if (mongoose.Error.CastError) {
+      //   next(new BadRequestError('Некорректный id карточки'));
+      // } else {
+      //   next(err);
+      // }
       // switch (err.constructor) {
       //   case (err.name === 'TypeError'):
         // case mongoose.Error.DocumentNotFoundError:
